31269d61c42c6b5d3ad4c7bfed5f80a95afa939e
nimesh9172 <nimeshdj123@gmail.com>
2026-03-28 22:06:35 +0530

    added home page

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -17,6 +18,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('api/', include('api.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
